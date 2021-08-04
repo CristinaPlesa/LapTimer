206df17bbf9493580282aa4d21b3b97a677831e2
Cristina Plesa <cristinaplesa8@gmail.com>
2021-08-04 00:14:54 -0700

    add useState and useEffectfunction to set millisecond intervals

---
+++ src/components/Timer.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useCallback, useMemo, useState } from 'react'
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonButton } from '@ionic/react';
+// import { msToClockUnits, msToClockString } from '../util/printTime'
+
+// Cristina
+const Timer: React.FC = () => {
+  const [ms, setMs] = useState(0)
+  const [isRunning, setIsRunning] = useState(false)
+
+  const tick = useCallback(async () => setMs(ms+1), [ms])
+
+  // isRunning &&
+  useEffect(() => {
+    const timerId = setInterval(
+      isRunning
+        ? tick
+        : () => undefined,
+      10
+    )
+    return () => clearInterval(timerId)
+  }, [isRunning, tick])
+
+  return (
+    <div>
+      <h1>Lap Timer</h1>
+      <IonButton>Start</IonButton>
+    </div>
+  )
+}
+
+export default Timer
